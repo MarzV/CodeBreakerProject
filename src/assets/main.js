f85d807197424f03c035e4f5c429e39782863f7c
MarzV <marz1966@me.com>
2017-02-04 10:15:13 +0100

    edit

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,18 +4,26 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer.value == "" && attempt.value =="") {
+        setHiddenFields()
+    }
 }
 
 //implement new functions here
 
 function setHiddenFields() {
     let amountZero = "";
+    attempt.value= "";
     answer.value= Math.floor((Math.random()*9999));
     answer.value.toString();
 
     for (let i = 4; answer.value.length < i; i--) {
         amountZero  += "0";
-
     }
+
     answer.value = amountZero + answer.value;
+}
+
+function setMessage(message) {
+    document.getElementById('message').innerHTML = message
 }
